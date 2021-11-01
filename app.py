8e49f5a707e69257046469d1438cc5ebfe080047
Akshay Bagai <akshay.bagai@instacart.com>
2021-11-01 16:28:08 -0400

    added authentication

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, jsonify, request, render_template
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'jose'
 api = Api(app)
 
-items = [
+jwt = JWT(app, authenticate, identity)
 
+items = [
+    {
+        "name": "item1",
+        "price": 10
+    }
 ]
 
 stores = [
@@ -22,6 +31,7 @@ stores = [
 
 # instead of a method at a time, you can have a resource and then bind urls with them
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
@@ -34,6 +44,12 @@ class Item(Resource):
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'item deleted'}
+
 
 class ItemsList(Resource):
     def get(self):
