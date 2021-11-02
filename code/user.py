9e5bdf77f4040f1686e498796cdb9b1c9796d524
Akshay Bagai <akshay.bagai@instacart.com>
2021-11-02 14:50:05 -0400

    implement user dao

--- code/user.py
+++ code/user.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask_restful import Resource, reqparse
 
 class User:
 
@@ -38,3 +39,35 @@ class User:
 
         connection.close()
         return user
+
+
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required= True,
+                        help="This field cannot be blank")
+
+    parser.add_argument('password',
+                        type=str,
+                        required= True,
+                        help="This field cannot be blank")
+
+    def post(self):
+
+        data = UserRegister.parser.parse_args();
+
+        if User.find_by_username(data['username']):
+            return {"message": "user already exists"}, 400
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "insert into users values (null, ?, ?)"
+        cursor.execute(query, (data['username'], data['password'],))
+
+        connection.commit()
+        connection.close()
+
+        return {"message": "user created successfully"}, 201
