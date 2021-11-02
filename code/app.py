c842375ef516bd0b6fa025046be406da73afa2c3
Akshay Bagai <akshay.bagai@instacart.com>
2021-11-02 15:49:37 -0400

    item dao

--- code/app.py
+++ code/app.py
@@ -4,6 +4,7 @@ from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
 from user import UserRegister
+from item import Item, ItemsList
 
 app = Flask(__name__)
 app.secret_key = 'jose'
@@ -30,49 +31,6 @@ stores = [
     }
 ]
 
-# instead of a method at a time, you can have a resource and then bind urls with them
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-                        type=float,
-                        required=True,
-                        help="This field is required")
-
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'item': item}, 200 if item else 404
-
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
-            return {'message', "An item with name {} already exits".format(name)}, 400
-
-        data = request.get_json()
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item, 201
-
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'item deleted'}
-
-    # PUT method is always idempotent
-    def put(self, name):
-
-        data = Item.parser.parse_args()
-
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-class ItemsList(Resource):
-    def get(self):
-        return {'item': items}
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemsList, '/items')
--- code/create_tables.py
+++ code/create_tables.py
@@ -7,6 +7,11 @@ cursor = connection.cursor()
 create_table = "create table if not exists users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
 
+items_table = "create table if not exists items (name text, price real)"
+cursor.execute(items_table)
+
+cursor.execute("insert into items values ('item1', 10.99)")
+
 connection.commit()
 
 connection.close()
